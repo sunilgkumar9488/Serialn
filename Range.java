b3f1dfddaa6920657924cdc40b6f08fc11328647
sunilgkumar9488 <sunilgkumar9488@gmail.com>
2016-08-12 09:27:09 +0530

    modified

--- Range.java
+++ Range.java
@@ -2,40 +2,40 @@
 public class Range {
     int low;
     int high;
-    
+
     public int getLow() {
-    	return low;
+        return low;
     }
-    
+
     public int getHigh() {
-    	return high;
+        return high;
     }
-    
-    public boolean iscontains(Range r) {
-    	return ((r.getLow()>this.low) && (r.getHigh()<this.high));
+
+    public boolean iscontains(TableEntries other) {
+        return ((this.low<other.low) && (this.high>other.high));
     }
-    
-    public boolean isless(Range r) {
-    	return (this.high<r.getLow());
+
+    public boolean isless(TableEntries other) {
+        return (other.high<this.low);
     }
-    
-    public boolean isgreater(Range r) {
-    	return (r.getHigh()<this.low);
+
+    public boolean isgreater(TableEntries other) {
+        return (other.low>this.high);
     }
-    
-    public boolean isequal(Range r) {
-    	return ((r.getHigh()==this.high)&&(r.getLow()==this.low));
+
+    public boolean isequal(TableEntries other) {
+        return ((other.high==this.high)&&(other.low==this.low));
     }
-    
-    public boolean isadjacent(Range r) {
-    	return ((r.getHigh()==this.low)||(r.getLow()==this.high));
+
+    public boolean isadjacent(TableEntries other) {
+        return ((other.high==this.low)||(other.low==this.high));
     }
-    
-    public boolean lowIntersects(Range r) {
-    	return ((r.getHigh()>this.low)&&(r.getLow()<this.low));
+
+    public boolean lowIntersects(TableEntries other) {
+        return ((other.high>this.low)&&(other.low<this.low));
     }
-    
-    public boolean highIntersects(Range r) {
-    	return ((r.getLow()<this.high)&&(r.getHigh()>this.high));
+
+    public boolean highIntersects(TableEntries other) {
+        return ((other.low<this.high)&&(other.high>this.high));
     }
 }
