823162cec82b8052fd1337b11be2cc41380b73df
sunilgkumar9488 <sunilgkumar9488@gmail.com>
2016-08-12 09:27:42 +0530

    modified

--- TableEntries.java
+++ TableEntries.java
@@ -1,12 +1,29 @@
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 /**
  * Created by gudkumar on 8/11/2016.
  */
-public class TableEntries {
-    List<Row> tableentries;
-    TableEntries(List<Row> rows)
+public class TableEntries extends Range{
+
+    private String s="";
+    private int level;
+
+    TableEntries(int low,int high,String s,int level)
     {
-        this.tableentries=rows;
+        this.low=low;
+        this.high=high;
+        this.s=s;
+        this.level=level;
     }
+
+    public String getS() {
+        return s;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
 }
